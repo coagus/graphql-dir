71f7d7b7219101d342cbe347e34a950f3932e09b
CHRISTIAN AGUSTIN <christian.agustin@ericsson.com>
2022-08-17 17:56:20 -0600

    :sparkles: Custom Types
    agrego custom types a los squemas

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql');
+const { buildSchema } = require('graphql')
 const express = require('express')
-const { graphqlHTTP } = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers.js')
 
 const app = express()
 const port = process.env.port || 3000
 
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`)
-
-const resolvers = {
-    hello: () => {
-        return 'Hola Mundo'
-    }
-}
+const schema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8'
+    )
+)
 
 app.use('/api', graphqlHTTP({
-    schema: schema,
+    schema,
     rootValue: resolvers,
     graphiql: true
 }))
@@ -29,14 +27,3 @@ app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}/api`)
 })
 
-// graphql({
-//     schema: schema,
-//     source: '{ saludo }',
-//     rootValue: resolvers
-// })
-//     .then((data) => {
-//         console.log(data);
-//     })
-//     .catch(e => {
-//         console.log(e);
-//     });
