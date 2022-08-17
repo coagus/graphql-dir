ad0a505ab235b2a139caf82dbc49452fc0e3a572
CHRISTIAN AGUSTIN <christian.agustin@ericsson.com>
2022-08-17 17:07:43 -0600

    :sparkles: API en la web
    agrego express para que sea visible en la web

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 'use strict'
 
 const { graphql, buildSchema } = require('graphql');
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express()
+const port = process.env.port || 3000
 
 const schema = buildSchema(`
     type Query {
         hello: String
-        saludo: String
     }
 `)
 
 const resolvers = {
     hello: () => {
         return 'Hola Mundo'
-    },
-    saludo: () => {
-        return 'Hola a Todos'
     }
 }
 
-graphql({
+app.use('/api', graphqlHTTP({
     schema: schema,
-    source: '{ saludo }',
-    rootValue: resolvers
+    rootValue: resolvers,
+    graphiql: true
+}))
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}/api`)
 })
-    .then((data) => {
-        console.log(data);
-    })
-    .catch(e => {
-        console.log(e);
-    });
+
+// graphql({
+//     schema: schema,
+//     source: '{ saludo }',
+//     rootValue: resolvers
+// })
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch(e => {
+//         console.log(e);
+//     });
